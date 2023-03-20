6806c36d4bad001176167892944d914fb0c196b4
LincolnMuraguri <lincolnmuraguri93@gmail.com>
2023-03-20 14:43:07 +0300

    Assignment done

--- README.md
+++ README.md
@@ -15,4 +15,11 @@ assign your output to the finalOutput variable
 
 
 # YOUR README DESCRIPTION STARTS HERE
+# Challenge 1: Student Grade Generator (Toy Problem)
+This challenge prompts the user to input student marks that are between 0 and 100. The program then outputs the correct grade based on the following grading system: A for marks above 79, B for marks between 60 and 79, C for marks between 49 and 59, D for marks between 40 and 49, and E for marks less than 40.
 
+# Challenge 2: Speed Detector (Toy Problem)
+The Speed Detector challenge requires a program that takes as input the speed of a car and determines whether the driver is over the speed limit. If the speed is less than or equal to 70 km/h, the program prints "Ok". However, if the speed is above 70 km/h, the program will issue one demerit point for every 5 km/h above the limit and print the total number of demerit points issued. If the driver receives more than 12 demerit points, the program will print "License suspended".
+
+# Challenge 3: Net Salary Calculator (Toy Problem)
+The Net Salary Calculator challenge requires a program that calculates an individual's net salary based on the inputs of their basic salary. The program will calculate the payee (tax), NHIF deductions, NSSFD deductions, gross salary, and net salary. The payee is calculated based on the individual's tax bracket and gross salary, while the NHIF and NSSFD deductions are fixed percentages of the individual's gross salary. The gross salary is calculated as the sum of the basic salary and benefits, and the net salary is calculated as the gross salary minus the payee, NHIF, and NSSFD deductions
--- script.js
+++ script.js
@@ -9,17 +9,27 @@ let inputGrade=parseInt(document.getElementById('input-grade').value)
 console.log(inputGrade)
 let finalOutput
 // write Code For the Code challenge
+let outputGrade;
+if (inputGrade <= 100 && inputGrade >= 80) {
+    outputGrade = "A";
+    
+} else if (inputGrade <= 79 && inputGrade >= 60) {
+    outputGrade = "B";
+    
+} else if (inputGrade <= 59 && inputGrade >= 50) {
+    outputGrade = "C";
+    
+} else if (inputGrade <= 49 && inputGrade >= 40) {
+    outputGrade = "D";
+    
+} else if (inputGrade <= 39 && inputGrade >= 0) {
+    outputGrade = "E";
+    
+} else {
+    outputGrade = 'Cannot input';
+}
 
-
-
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
+finalOutput = outputGrade
 document.getElementById('marks-id').append(finalOutput)
 
 })
@@ -29,20 +39,21 @@ speed.addEventListener('click', (e)=>{
     console.log(inputSpeed)
     let finalOutput
 // write Code For the Code challenge
+let speed;
+let demeritPoints = 0;
+let output;
+if (inputSpeed < 70) {
+  output = "Ok";
+} else {
+  let demeritPoints = Math.floor((inputSpeed-70)/5)
+  if (demeritPoints>=12) {
+    output = "License suspended"
+  }else{
+    output = `Points: ${demeritPoints}`
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
+finalOutput= output
     document.getElementById('speed-id').append(finalOutput)
 
 })
@@ -52,18 +63,62 @@ salary.addEventListener('click', (e)=>{
         console.log(inputSalary)
 let finalOutput
 // write Code For the Code challenge
+function netPayCalculator(gPay) {
+  let grossPay = gPay - 2400; 
+  let paye = 0;       
+  let nssf = gPay * 0.06;        
+  let nhif = 0;         
+  // compute PAYE based on taxable income
+  if (grossPay <= 24000) {
+      paye = grossPay * 0.1
+  } else if (grossPay >= 24001 && grossPay <= 32333) {
+      paye = grossPay * 0.25
+  } else if (grossPay > 32333) {
+      paye = grossPay * 0.3
+  }
+  // based on gross salary
+  if (gPay <= 5999) {
+      nhif = 150;
+  } else if (gPay <= 7999) {
+      nhif = 300;
+  } else if (gPay <= 11999) {
+      nhif = 400;
+  } else if (gPay <= 14999) {
+      nhif = 500;
+  } else if (gPay <= 19999) {
+      nhif = 600;
+  } else if (gPay <= 24999) {
+      nhif = 750;
+  } else if (gPay <= 29999) {
+      nhif = 850;
+  } else if (gPay <= 34999) {
+      nhif = 900;
+  } else if (gPay <= 39999) {
+      nhif = 950;
+  } else if (gPay <= 44999) {
+      nhif = 1000;
+  } else if (gPay <= 49999) {
+      nhif = 1100;
+  } else if (gPay <= 59999) {
+      nhif = 1200;
+  } else if (gPay <= 69999) {
+      nhif = 1300;
+  } else if (gPay <= 79999) {
+      nhif = 1400;
+  } else if (gPay <= 89999) {
+      nhif = 1500;
+  } else if (gPay <= 99999) {
+      nhif = 1600;
+  } else if (gPay > 100000) {
+      nhif = 1700;
+  }
+  // calculate the net salary
+  let netPay = gPay - paye - nssf - nhif;
+  return netPay
+}
 
 
-
-
-
-
-
-
-
-
-
-finalOutput ='assign your final output to this variable'
+finalOutput =netPayCalculator(inputSalary)
 document.getElementById('salary-id').append(finalOutput)
         
 })
